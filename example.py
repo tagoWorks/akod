590404319e1321690a8b0c5a988d09e9e2c4a43c
tago <itslocaltago@gmail.com>
2024-03-17 17:33:08 -0700

    create example

--- example.py
+++ example.py
@@ -1,3 +1,11 @@
-import tkinter as tk
-from tkinter import messagebox
-messagebox.showinfo("Success", "The key was right and this was ran")
+import VLoDVP
+VLoDVP.setkey(b'12345678901234567890123456789012')
+VLoDVP.setlink('https://yourlink.netlify.app/')
+
+emailvar = input("Enter email: ")
+licensekeyvar = input("Enter key: ")
+
+if VLoDVP.validate(emailvar, licensekeyvar) == False:
+    print("Invalid email or key")
+else:
+    print("Valid email and key")
