cd1efdf3518153a15fb47616e03ed3eb90c2ada2
tago <78416380+t-a-g-o@users.noreply.github.com>
2024-04-20 23:10:39 -0700

    Update example.py

--- example.py
+++ example.py
@@ -1,24 +1,26 @@
-import AKoDAuth as auth
+import requests
 import os
-auth.privatekey('private key from identifiers.txt')
-auth.publicserverkey('public key from identifiers.txt')
+
+private_key = "private key from identifiers.txt"
+publicserverkey = "public key from identifiers.txt"
+requests.post('http://yourusername.pythonanywhere.com/privatekey', data={'privatekey': private_key})
+requests.post('http://yourusername.pythonanywhere.com/publickey', data={'link': publicserverkey})
+
 
 if os.path.exists('licensekey.txt'):
     with open ('licensekey.txt', 'r') as f:
-        auth.setActivationKey(f.read())
+        activationkey = f.read()
         f.close()
 else:
     activationkey = input("Enter activation key: ")
-    auth.setActivationKey(activationkey)
     with open ('licensekey.txt', 'w') as f:
         f.write(activationkey)
         f.close()
 
 username = input("Enter username: ")
 password = input("Enter password: ")
+requests.post('http://yourusername.pythonanywhere.com/setactivationkey', data={'key': activationkey})
+response = requests.post('http://yourusername.pythonanywhere.com/validate', data={'username': username, 'password': password})
 
-if auth.isValid(username, password) == False:
-    print("Invalid login credentials.")
-    exit
-else:
-    print("Hello World!")
+if response.text == "VALID":
+    print("Hello, World!")
